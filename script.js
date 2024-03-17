f174f6157be63c84cb797c7cde6ae9b95092da09
Ashwin Kumar <135273362+Ashwin723@users.noreply.github.com>
2024-03-17 16:40:07 +0530

    Create script.js

---
+++ script.js
@@ -0,0 +1,25 @@
+function executeCurl() {
+  var curlInput = document.getElementById('curlInput').value;
+  var curlOutput = document.getElementById('curlOutput');
+
+  // Clear previous output
+  curlOutput.innerHTML = '';
+
+  // Execute the curl command
+  fetch('https://api.example.com/execute-curl', {
+    method: 'POST',
+    headers: {
+      'Content-Type': 'application/json'
+    },
+    body: JSON.stringify({ curlCommand: curlInput })
+  })
+  .then(response => response.json())
+  .then(data => {
+    // Display output
+    curlOutput.innerHTML = '<pre>' + JSON.stringify(data, null, 2) + '</pre>';
+  })
+  .catch(error => {
+    console.error('Error executing curl command:', error);
+    curlOutput.innerHTML = '<span style="color: red;">Error executing curl command. Please try again.</span>';
+  });
+}
